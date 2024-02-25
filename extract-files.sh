82b2d1291aa89ab85a0460696b2f58179b6202cd
Fyramiz <160392615+Fyramiz@users.noreply.github.com>
2024-02-25 13:55:06 +0100

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -15,7 +15,7 @@ VENDOR=incar
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="$croot"
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
